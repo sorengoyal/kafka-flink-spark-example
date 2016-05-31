916b6745584ac9f67182eb5225fed52dce246000
sogoyal <sogoyal@expedia.com>
2016-05-31 11:13:30 -0500

    Rewrote the producer to conform to the kafka 0.8.2 definition

--- src/main/scala/com/expedia/www/Producer.scala
+++ src/main/scala/com/expedia/www/Producer.scala
@@ -4,33 +4,25 @@ package com.expedia.www
   * Created by sogoyal on 5/24/16.
   */
 
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerRecord
 import java.util.Properties
 
+import kafka.producer.KeyedMessage
+import kafka.producer.ProducerConfig
 
-class Producer(topic: String, isAsync: Boolean) extends Thread {
+class Producer(topic: String, isAsync: Boolean) extends Thread
+{
   var messageNo: Int = 0
-  val props = new Properties()
-  props.put("bootstrap.servers", "localhost:9092")
-  props.put("client.id", "DemoProducer")
-  props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer")
-  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  val props: Properties  = new Properties()
+  props.put("serializer.class", "kafka.serializer.StringEncoder")
+  props.put("metadata.broker.list", "localhost:9092")
+  // Use random partitioner. Don't need the key type. Just set it to Integer.
+  // The message is of type String.
+  val producer: kafka.javaapi.producer.Producer[Integer, String] = new kafka.javaapi.producer.Producer[Integer, String](new ProducerConfig(props))
 
-  val kproducer = new KafkaProducer[Int, String](props)
   def run(message: String): Unit = {
-    val record = new ProducerRecord(topic, messageNo, message)
-    if (isAsync) { // Send asynchronously
-      kproducer.send(record)
+    while(true) {
+      producer.send(new KeyedMessage[Integer, String](topic, message))
+      messageNo += 1
     }
-    else {
-      // Send synchronously
-      kproducer.send(new ProducerRecord(topic, messageNo, message)).get()
-      println("Sent message: (" + messageNo + ", " + message + ")")
-    }
-    messageNo += 1
-  }
-  override def toString() = {
-    topic
   }
 }
